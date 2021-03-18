4ad0289079502e1e8f08e9b1f1438d17dc296414
Marcelino <santanatruocchio@gmail.com>
2021-03-18 10:11:21 -0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Marcelino Santana Truocchio on 18/03/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
